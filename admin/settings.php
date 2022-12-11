3ded97a25a8588612bd56f1df6bb6bd7f22bac37
munna <freelancermunna4@gmail.com>
2022-12-11 18:55:30 +0600

    update website setting

--- admin/settings.php
+++ admin/settings.php
@@ -1,189 +1,223 @@
 <?php include("common/header-sidebar.php")?>
-      <div style="padding-left: 0;" class="x_container flex items-start relative">
-        <div style="width:300px;" class="h-screen sticky top-0 left-0 right-0 hidden md:block">
-          <ul
-            class="-mr-[1px] h-fit bg-gray-100 sticky top-[80px] left-0 border border-r-0 border-gray-200 border-r-transparent rounded-l overflow-hidden">
-            <a href="./settings.php"
-              class="p-4 gap-x-3 flex items-center border-b border-gray-200 bg-white cursor-pointer text-base font-medium text-cyan-800">
-              <span><i class="fa-solid fa-screwdriver-wrench"></i></span>
-              <span>General Setting</span>
-            </a>
+<?php include("common/setting-sidebar.php")?>
+      
+        <?php
+        $data = _fetchAll("website");
 
-            <a href="./payment-gateway.php"
-              class="p-4 gap-x-3 flex items-center border-b border-gray-200 hover:bg-white cursor-pointer text-base font-medium text-cyan-800">
-              <span class="text-purple-600"><i class="fa-brands fa-amazon-pay"></i></span>
-              <span>Payment Gateway</span>
-            </a>
+        if(isset($_POST['submit'])){
+            $title = $_POST['title'];
+            $logo = $_POST['logo'];
+            $description = $_POST['description'];
+            $keyword = $_POST['keyword'];
+            $url = $_POST['url'];
+            $phone = $_POST['phone'];
+            $mail = $_POST['mail'];
+            $address = $_POST['address'];
+            $footer_text = $_POST['footer_text'];
+            $facebook = $_POST['facebook'];
+            $youtube = $_POST['youtube'];
+            $linkedin = $_POST['linkedin'];
 
-            <a href="./limit-setting.php"
-              class="p-4 gap-x-3 flex items-center border-b border-gray-200 hover:bg-white cursor-pointer text-base font-medium text-cyan-800">
-              <span class="text-red-600"><i class="fa-solid fa-chart-line"></i></span>
-              <span>Limit Setting</span>
-            </a>
-            <a href="./deposit-bank.php"
-              class="p-4 gap-x-3 flex items-center border-b border-gray-200 hover:bg-white cursor-pointer text-base font-medium text-cyan-800">
-              <span class="text-gray-700"><i class="fa-solid fa-building-columns"></i></span>
-              <span>Deposit Bank</span>
-            </a>
-            <a href="./withdrawl-bank.php"
-              class="p-4 gap-x-3 flex items-center border-b border-gray-200 hover:bg-white cursor-pointer text-base font-medium text-cyan-800">
-              <span class="text-pink-500"><i class="fa-solid fa-vault"></i></span>
-              <span>Withdrawl Bank</span>
-            </a>
-            <a href="./mailing-setting.php"
-              class="p-4 gap-x-3 flex items-center border-b border-gray-200 hover:bg-white cursor-pointer font-medium text-cyan-800">
-              <span class="text-orange-600"><i class="fa-brands fa-mailchimp"></i></span>
-              <span>Mailing Setting</span>
-            </a>
-            <a href="./all-notice-setting.php"
-              class="p-4 gap-x-3 flex items-center border-b border-gray-200 hover:bg-white cursor-pointer font-medium text-cyan-800">
-              <span class="text-blue-600"><i class="fa-solid fa-circle-info"></i></span>
-              <span>All Notice Setting</span>
-            </a>
-            <a href="./adsense.php"
-              class="p-4 gap-x-3 flex items-center hover:bg-white cursor-pointer font-medium text-cyan-800">
-              <span class="text-orange-600"><i class="fa-brands fa-google"></i></span>
-              <span>Adsense</span>
-            </a>
-          </ul>
-        </div>
+            $update = _update("website","title='$title',logo='$logo',description='$description',keyword='$keyword',url='$url',phone='$phone',mail='$mail',address='$address',footer_text='$footer_text',facebook='$facebook',youtube='$youtube',linkedin='$linkedin'","id=1");
 
+            if($update){
+              $msg = "Update Successfully";
+              header("location:settings.php?msg=$msg");
+            }else{
+              echo $err = "Something is wrong";
+            }
+        }
+        
+        
+        
+        
+        ?>
         <div class="w-full space-y-10 p-6 lg:p-12 bg-white border border-gray-200 rounded">
-          <form class="grid grid-cols-2 gap-y-6 gap-x-12">
+          <form action="" method="POST" class="grid grid-cols-2 gap-y-6 gap-x-12">
             <div class="col-span-2">
               <h2 class="text-xl font-semibold text-cyan-800">Website Info</h2>
             </div>
 
             <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
-              <label for="Site Title">Site Title</label>
-              <input class="input" type="text" id="Site Title" placeholder="Site Title" required>
+              <label for="title">Site Title</label>
+              <input name="title" class="input" type="text" id="title" placeholder="Site Title" required value="<?php echo $data['title']?>">
             </div>
 
 
             <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
-              <label for="Site Logo">Site Logo</label>
-              <input class="input" type="text" id="Site Logo" placeholder="Site Logo" required>
+              <label for="logo">Site Logo</label>
+              <input name="logo" class="input" type="text" id="logo" placeholder="Site Logo" required value="<?php echo $data['logo']?>">
+            </div>
+
+            <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
+              <label for="url">Site URL</label>
+              <input name="url" class="input" type="text" id="url" placeholder="Url" required value="<?php echo $data['url']?>">
             </div>
 
 
             <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
-              <label for="Site Description">Site Description <small>(Website Meta Description)</small> </label>
-              <input class="input" type="text" id="Site Description" placeholder="Site Description" required>
-            </div>
-
-            <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
-              <label for="Site Keywords">Site Keywords <small>(Meta Keywords)</small> </label>
-              <input class="input" type="text" id="Site Keywords" placeholder="Site Keywords" required>
+              <label for="phone">Phone</label>
+              <input name="phone" class="input" type="text" id="phone" placeholder="Phone number" required value="<?php echo $data['phone']?>">
             </div>
 
 
             <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
-              <label for="Site URL">Site URL</label>
-              <input class="input" type="text" id="Site URL" placeholder="Site URL" required>
+              <label for="mail">Mail</label>
+              <input name="mail" class="input" type="text" id="mail" placeholder="Mail" required value="<?php echo $data['mail']?>">
             </div>
 
 
-            <div class="col-span-2 lg:col-span-1col-span-2 lg:col-span-1 flex flex-col gap-y-1">
-              <label for="default_language">Default Language</label>
-              <select class="select" name="" id="default_language" required>
-                <option value="english">English</option>
-                <option value="bengali">Bengali</option>
-              </select>
+            <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
+              <label for="address">Address</label>
+              <input name="address" class="input" type="text" id="address" placeholder="Address" required value="<?php echo $data['address']?>">
             </div>
+
+
+            <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
+              <label for="footer_text">Fotter Text</label>
+              <input name="footer_text" class="input" type="text" id="footer_text" placeholder="Footer Text" required value="<?php echo $data['footer_text']?>">
+            </div>
+
+
+            <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
+              <label for="facebook">Facebook</label>
+              <input name="facebook" class="input" type="text" id="facebook" placeholder="Facebook Page" required value="<?php echo $data['facebook']?>">
+            </div>
+
+
+            <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
+              <label for="youtube">Youtube</label>
+              <input name="youtube" class="input" type="text" id="youtube" placeholder="Youtube Channel" required value="<?php echo $data['youtube']?>">
+            </div>
+
+
+            <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
+              <label for="linkedin">Linkedin</label>
+              <input name="linkedin" class="input" type="text" id="linkedin" placeholder="Linkedin Page" required value="<?php echo $data['linkedin']?>">
+            </div>
+
+            <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
+              <label for="description">Description</label>
+              <textarea name="description" class="input" id="description" cols="30" rows="10"><?php echo $data['description']?></textarea>
+            </div>
+
+
+            <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
+              <label for="keyword">Keyword</label>
+              <textarea name="keyword" class="input" id="keyword" cols="30" rows="10"><?php echo $data['keyword']?></textarea>
+            </div>
+
 
             <div class="col-span-2 flex justify-start">
               <div class="w-fit">
-                <button class="button">Submit</button>
+                <button type="submit" name="submit" class="button">Save</button>
               </div>
             </div>
 
           </form>
-          <hr class="my-6" />
-          <form class="grid grid-cols-2 gap-y-6 gap-x-12">
-            <div class="col-span-2">
-              <h2 class="text-xl font-semibold text-cyan-800">Registration Settings</h2>
-            </div>
+<br>
+<br>
+          <hr>
 
-            <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
-              <label for="Coins on Signup">Coins on Signup <small>(added after signup)</small> </label>
-              <input class="input" type="text" id="Coins on Signup" placeholder="Coins on Signup" required>
-            </div>
+          <div style="display:flex;justify-content:space-around">
 
 
-            <div class="col-span-2 lg:col-span-1col-span-2 lg:col-span-1 flex flex-col gap-y-1">
-              <label for="1 Account Per IP">1 Account Per IP</label>
-              <select class="select" name="" id="1 Account Per IP" required>
-                <option value="Yes">Yes</option>
-                <option value="No">No</option>
-              </select>
-            </div>
+              <?php 
+              if(isset($_POST['add_logo'])){
+                $file_name = $_FILES['file']['name'];
+                $file_tmp = $_FILES['file']['tmp_name'];
+                move_uploaded_file($file_tmp,"upload/$file_name");
+                if(empty($_file_name)){
+                  $msg = "Please Select File";
+                  header("location:settings.php?msg=$msg");
+                }else{
+                $update = _update("website","file_name='$file_name'","id=1");
+                if($update){
+                  $msg = "Successfully Updated";
+                  header("location:settings.php?msg=$msg");
+                }
+                else{
+                  $err = "Something is wrong";
+                }
+              }
+              }elseif(isset($_POST['remove_logo'])){
+                $update = _update("website","file_name=''","id=1");
+                if($update){
+                  $msg = "Successfully Removed";
+                  header("location:settings.php?msg=$msg");
+                }
+              }              
+              ?>
+              <div>
+                <form action="" method="POST" enctype="multipart/form-data">
+                <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
+                  <label for="logo_image">Logo Image</label>
+                  <?php if(empty($data['file_name'])){}else{?>
+                  <img style="width:300px;height:100px;margin:10px auto;border-radius:10px;" src="upload/<?php echo $data['file_name']?>">
+                  <?php }?>
+                  <input style="padding-top:10px" name="file" class="input" type="file" id="logo_file">
+                </div>
 
-            <div class="col-span-2 lg:col-span-1col-span-2 lg:col-span-1 flex flex-col gap-y-1">
-              <label for="Email Confirmation">Email Confirmation</label>
-              <select class="select" name="" id="Email Confirmation" required>
-                <option value="Disabled">Disabled</option>
-                <option value="Enabled">Enabled</option>
-              </select>
-            </div>
-
-            <div class="col-span-2 flex justify-start">
-              <div class="w-fit">
-                <button class="button">Submit</button>
+                <div class="col-span-2 flex justify-start">
+                  <div class="w-fit" style="display:flex;gap:10px;padding:20px 0">
+                    <button type="submit" name="add_logo" class="button">Add</button>
+                    <button type="submit" name="remove_logo" class="button">Remove</button>
+                  </div>
+                </div>
+                </form>
               </div>
-            </div>
 
-          </form>
-          <hr class="my-6" />
-          <form class="grid grid-cols-2 gap-y-6 gap-x-12">
-            <div class="col-span-2">
-              <h2 class="text-xl font-semibold text-cyan-800"> Withdrawal Settings </h2>
-            </div>
 
-            <div class="col-span-2 lg:col-span-1col-span-2 lg:col-span-1 flex flex-col gap-y-1">
-              <label for="Payment Currency">Payment Currency</label>
-              <select class="select" name="" id="Payment Currency" required>
-                <option value="USD">USD</option>
-                <option value="EUR">EUR</option>
-                <option value="BDT">BDT</option>
-                <option value="INR">INR</option>
-              </select>
-            </div>
+              <?php 
+              if(isset($_POST['add_favicon'])){
+                $file_name = $_FILES['file']['name'];
+                $file_tmp = $_FILES['file']['tmp_name'];
+                move_uploaded_file($file_tmp,"upload/$file_name");
+                if(empty($_file_name)){
+                  $msg = "Please Select File";
+                  header("location:settings.php?msg=$msg");
+                }else{
+                $update = _update("website","favicon_name='$file_name'","id=1");
+                if($update){
+                  $msg = "Successfully Updated";
+                  header("location:settings.php?msg=$msg");
+                }
+                else{
+                  $err = "Something is wrong";
+                }
+              }
+              }elseif(isset($_POST['remove_favicon'])){
+                $update = _update("website","favicon_name=''","id=1");
+                if($update){
+                  $msg = "Successfully Removed";
+                  header("location:settings.php?msg=$msg");
+                }
+              }              
+              ?>
+              <div>
+              <form action="" method="POST" enctype="multipart/form-data">
+                <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
+                  <label for="description">Favicon Image</label>
+                  <?php if(empty($data['favicon_name'])){}else{?>
+                  <img style="width:300px;height:100px;margin:10px auto;border-radius:10px;" src="upload/<?php echo $data['favicon_name']?>">
+                  <?php }?>
+                  <input style="padding-top:10px" name="file" class="input" type="file" id="logo_file">
+                </div>
 
-            <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
-              <label for="Conversion Rate">Conversion Rate <small>(How many coins for $1.00)</small> </label>
-              <input class="input" type="text" id="Conversion Rate" placeholder="Conversion Rate" required>
-            </div>
-
-            <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
-              <label for="Minimum Withdrawal">Minimum Withdrawal </label>
-              <input class="input" type="text" id="Minimum Withdrawal" placeholder="Minimum Withdrawal" required>
-            </div>
-
-            <div class="col-span-2 flex justify-start">
-              <div class="w-fit">
-                <button class="button">Submit</button>
+                <div class="col-span-2 flex justify-start">
+                  <div class="w-fit" style="display:flex;gap:10px;padding:20px 0">
+                    <button type="submit" name="add_favicon" class="button">Add</button>
+                    <button type="submit" name="remove_favicon" class="button">Remove</button>
+                  </div>
+                </div>
+                </form>
               </div>
-            </div>
+          </div>
 
-          </form>
-          <hr class="my-6" />
-          <form class="grid grid-cols-2 gap-y-6 gap-x-12">
-            <div class="col-span-2">
-              <h2 class="text-xl font-semibold text-cyan-800"> Referral System </h2>
-            </div>
 
-            <div class="col-span-2 lg:col-span-1 flex flex-col gap-y-1">
-              <label for="Comission">User Commission <small>(% of coins earned by referral)</small></label>
-              <input class="input" type="text" id="Comission" placeholder="10" required>
-            </div>
 
-            <div class="col-span-2 flex justify-start">
-              <div class="w-fit">
-                <button class="button">Submit</button>
-              </div>
-            </div>
 
-          </form>
+
         </div>
       </div>
     </div>
